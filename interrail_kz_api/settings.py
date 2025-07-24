da28fcb196e78909a6f0134c9c4311dad61ef64c
izzat1998 <izzattilla706@gmail.com>
2025-07-24 10:28:00 +0500

    ci cd workflow

--- interrail_kz_api/settings.py
+++ interrail_kz_api/settings.py
@@ -33,12 +33,13 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1,"])
 
 # CSRF Settings
-CSRF_TRUSTED_ORIGINS = env.list(
-    "CSRF_TRUSTED_ORIGINS", default=["http://localhost:8000", "http://127.0.0.1:8000"]
-)
+CSRF_TRUSTED_ORIGINS = [
+    "https://systemkz.interrail.uz/",
+    "https://system-kz.interrail.uz",
+]
 
 # Application definition
 
